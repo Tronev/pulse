0a28a152c28f2745c953ddff2ce6d5bd6e2fff32
Tronev Ivan <74rus_86@mail.ru>
2021-03-07 00:16:29 +0500

    Доверстали страничку

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,57 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         // adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="left"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="right"></button>',
-//         responsive: [
-//             {
-//               breakpoint: 992,
-//               settings: {
-//                 dots: true,
-//                 arrows: false
-//               }
-//             },
-//         ]
-//         });
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="right"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: false,
+                arrows: false
+              }
+            },
+        ]
+        });
+
+		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_activ)', function() {
+			$(this)
+			  .addClass('catalog__tab_activ').siblings().removeClass('catalog__tab_activ')
+			  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+		  });
+
+		function toggleSlide(item) {
+			$(item).each(function(i){
+				$(this).on('click', function(e){
+					e.preventDefault();
+					$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_activ');
+					$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_activ');
+				})
+			});
+		};
+
+		toggleSlide('.catalog-item__link');
+		toggleSlide('.catalog-item__back');
+  });
+
+// тини слайдер 
+
+// const slider = tns({
+// 	container: '.carousel__inner',
+// 	items: 1,
+// 	slideBy: 'page',
+// 	autoplay: false,
+// 	nav: false,
+// 	controls: false
+
+// });
+
+// document.querySelector('.prev').addEventListener('click', function () {
+// 	slider.goTo('prev');
 //   });
-
-
-const slider = tns({
-	container: '.carousel__inner',
-	items: 1,
-	slideBy: 'page',
-	autoplay: false,
-	nav: false,
-	controls: false
-
-});
-
-document.querySelector('.prev').addEventListener('click', function () {
-	slider.goTo('prev');
-  });
-document.querySelector('.next').addEventListener('click', function () {
-	slider.goTo('next');
-  });
+// document.querySelector('.next').addEventListener('click', function () {
+// 	slider.goTo('next');
+//   });
 
